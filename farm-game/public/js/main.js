d6c066cf071b9e312319ac21578f56f67c9c4aa7
Kevin Glick <kevin.n.glick@gmail.com>
2017-04-25 08:27:32 -0400

    add additional sprite images

--- farm-game/public/js/main.js
+++ farm-game/public/js/main.js
@@ -2,10 +2,16 @@ var game = new Phaser.Game(640, 360, Phaser.AUTO);
 
 const GameState = {
   preload() {
-
+    this.load.image('background', '../assets/images/background.png');
+    this.load.image('chicken', '../assets/images/chicken.png');
+    this.load.image('horse', '../assets/images/horse.png');
+    this.load.image('pig', '../assets/images/pig.png');
+    this.load.image('sheep', '../assets/images/sheep3.png');
   },
   create() {
-
+    this.background = this.game.add.sprite(0, 0, 'background');
+    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+    this.chicken.anchor.setTo(0.5, 0.5);
   },
   update() {
 
